8d44199ded73e37a3922a509cfae95fcf6d1b840
xoakzxz <juandavid_ja@hotmail.com>
2017-09-12 21:37:47 -0500

    Carga correcta de escenas

---
+++ Assets/Scripts/SceneLoader/Loading.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Loading : MonoBehaviour {
+
+    #region Properties
+
+    [SerializeField]
+    private GameObject loadingCanvas;
+
+    #endregion    
+
+    #region Unity functions
+
+    private void OnLevelWasLoaded(int level)
+    {
+        loadingCanvas.SetActive(false);
+    }
+
+    #endregion
+
+    #region Class functions
+
+    public void ActiveLoading()
+    {
+        loadingCanvas.SetActive(true);
+    }
+
+    #endregion
+}
